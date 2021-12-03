b097b347b3bd82464507d216c32909cc8288ddd9
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-03 19:27:58 +0300

    fixed bugs after comments

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
 const Schema = mongoose.Schema;
-
 
 const taskScheme = new Schema({
   text: String,
@@ -16,9 +14,9 @@ const Task = mongoose.model("tasks", taskScheme);
 app.use(cors());
 
 const url = 'mongodb+srv://ShishkunovD:restart987!@cluster0.8tbt5.mongodb.net/Todo-list-JS?retryWrites=true&w=majority';
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(url);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/allTasks', (req, res) => {
   Task.find().then(result => {
@@ -29,27 +27,45 @@ app.get('/allTasks', (req, res) => {
 app.post('/createTask', (req, res) => {
   const task = new Task(req.body);
   task.save().then(result => {
-    res.send('Task created');
+    res.send({data: result});
   })
 })
 
 app.delete('/deleteTask', (req, res) => {
   const identifier = req.query.id;
-  Task.deleteOne({_id : identifier}).then(result => {
-    res.send('Task deleted');
-  });
+  if(identifier === undefined) {
+    res.send("Пожалуйста, заполните id")
+  } else {
+    Task.deleteOne({_id : identifier})
+    .then(result => {
+      console.log('Task deleted');
+    })
+    Task.find().then(result => {
+      res.send({data: result});
+    })
+  }
 })
 
-app.put('/updateTask', (req, res) => {
-  const task = req.body._id;
-  Task.findByIdAndUpdate(task, { text : req.body.text }).then(result => {
-    res.send('Task was updated');
-  })
+app.patch('/updateTask', (req, res) => {
+  const params = req.query.id;
+  const body = req.body;
+  if(req.body.text === undefined) {
+    res.send("Пожалуйста, заполните текст")
+  } else {
+    Task.findByIdAndUpdate(params, { text: body.text })
+    .then(result => {
+      console.log('Task was updated');
+    })
+    Task.find().then(result => {
+      res.send({data: result});
+    })
+  }
 })
 
 app.put('/changeCheckbox', (req, res) => {
-  const task = req.body._id;
-  Task.findByIdAndUpdate(task, {isCheck: req.body.isCheck}).then(result => {
+  const body = req.body;
+  const task = body._id;
+  Task.findByIdAndUpdate(task, {isCheck: body.isCheck}).then(result => {
     res.send('Checkbox was changed');
   })
 })
