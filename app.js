c9cd08e6ad713bc9ae6f00f8ee8a12e9caa8093f
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-06 12:30:55 +0300

    fixed bugs after the second comments

--- app.js
+++ app.js
@@ -26,15 +26,19 @@ app.get('/allTasks', (req, res) => {
 
 app.post('/createTask', (req, res) => {
   const task = new Task(req.body);
-  task.save().then(result => {
-    res.send({ data: result });
-  })
+  if(req.body.text === undefined || typeof req.body.isCheck !== "boolean") {
+    res.status(404).send("Error, all fields should be filled in correctly.");
+  } else {
+    task.save().then(result => {
+      res.send({ data: result });
+    })
+  }
 })
 
 app.delete('/deleteTask', (req, res) => {
   const identifier = req.query.id;
   if(identifier === undefined) {
-    res.send("Пожалуйста, заполните id.");
+    res.status(404).send("Error, please enter the id.");
   } else {
     Task.deleteOne({ _id : identifier })
     .then(result => {
@@ -45,13 +49,11 @@ app.delete('/deleteTask', (req, res) => {
 
 app.patch('/updateTask', (req, res) => {
   const params = req.query.id;
-  const body = req.body.text;
-  if(body === undefined) {
-    res.send("Пожалуйста, заполните поле текста.");
-  } else if(params === undefined) {
-    res.send("Пожалуйста, заполните id.");
+  const body = req.body;
+  if(body.text === undefined || params === undefined || typeof body.isCheck !== "boolean") {
+    res.status(404).send("Error, all fields should be filled.");
   } else {
-    Task.findByIdAndUpdate(params, { text: body })
+    Task.findByIdAndUpdate(params, { text: body.text })
     .then(result => {
       Task.find().then(result => res.send({ data: result }))
     })
